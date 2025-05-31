192e673aea22c9de36845febc6faa6b4ab5b5460
Rifqi Fathoni <itsmeriee02@gmail.com>
2025-05-31 11:44:05 +0800

    Operasi Penuh(Full)

--- C030324042stack.cpp
+++ C030324042stack.cpp
@@ -20,3 +20,11 @@ int Empty(Stack *S){
     return(S->count==0);
 }
 
+void Pop(Stack *S,ItemType *X){
+    if(S->count == 0){
+        cout<<"Stack nya masih kosong mas"<<endl;
+    } else {
+        --(S->count);
+        *X=S->Item[S->count];
+    }
+}
